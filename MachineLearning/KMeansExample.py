2047bb8267f8f25c9b288575711f243cd2a10691
unknown <caiosgon3@gmail.com>
2018-11-03 17:37:40 -0300

    Kmeans removing unused imports

--- MachineLearning/KMeansExample.py
+++ MachineLearning/KMeansExample.py
@@ -1,14 +1,9 @@
 import pandas as pd
 from KMeans import KMeans
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import numpy as np
-from ArrayUtil import one_dim_array_reshape
-
-# file_name = "DataSets/LinearRegression/ex1data1.txt"
-# data = pd.read_csv(file_name, names=['X', 'Y'])
 from scipy.io import loadmat
+
 mat = loadmat('DataSets/KMeans_PCA/ex7data2.mat')
 data = mat['X']
 
