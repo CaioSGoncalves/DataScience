594152351db0642ba9d26b23c47d8eeedff0a20a
Caio <caiosgon3@gmail.com>
2019-03-13 10:01:42 -0300

    Deleting Folder

--- TCC/load_mongo.py
+++
@@ -1,7 +0,0 @@
-from Loaders.mongo_loader import Mongo_Loader
-
-loader = Mongo_Loader(host_name='localhost')
-
-# query parameter format = term + '+language:'+ language
-# loader.collect_data(query='language:python', number_of_pages=1)
-# loader.insert_data(db_name='github_twitter')
